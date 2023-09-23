6f72ba6e5ca12118761d9461f2a6043899785080
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-09-23 13:00:37 +0100

    Update TextAlignJustify.js
    completed

--- CW5kyu/CW4kyu/TextAlignJustify.js
+++ CW5kyu/CW4kyu/TextAlignJustify.js
@@ -49,5 +49,26 @@
 
 // My Solution:
 
-
+function justify(str, len) {
+  var words = str.split(' ');
+  var lines = [];
+  var lastLine = words.reduce(function(line, word) {
+    if (line) {
+      if (line.length + word.length + 1 <= len)
+        return line + ' ' + word;
+      lines.push(line);
+    }
+    return word;
+  });
+  lines = lines.map(function(line) {
+    if (line.indexOf(' ') >= 0)
+      for (var lineLen = line.length; lineLen < len; )
+        line = line.replace(/ +/g, function(spaces) {
+          return spaces + (lineLen++ < len ? ' ' : '');
+        });
+    return line;
+  });
+  lastLine && lines.push(lastLine);
+  return lines.join('\n');
+}
 
